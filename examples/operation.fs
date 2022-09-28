198227376a775d6695a8b5864d7adea9f76e70d5
Dermecy <112899609+Dermecy@users.noreply.github.com>
2022-09-28 16:25:49 +0545

    Update operation.fs

--- examples/operation.fs
+++ examples/operation.fs
@@ -6,19 +6,24 @@ function main zero {
    c = number(scanf("second number : "));
    if (b == "+") {
     printf(a + c);
+    printf("\n");
    };
    if (b == '-') {
     printf(a - c);
+    printf("\n");
    };
    if (b == '/') {
     printf(a / c);
+    printf("\n");
    };
    if (b == '*') {
     printf(a*c);
+    printf("\n")
    };
    a = 1;
   };
   if (a == 0) {
     printf("Invaild Numbers")
+    printf("\n");
   };
 };
